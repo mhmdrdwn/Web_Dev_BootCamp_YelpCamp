f0659408260deef3429d9c70ce5f2b185565d31c
mhmdrdwn <mhmdrdwn88@gmail.com>
2019-02-12 21:35:09 +0100

    Adding data to mongoDB

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 var express = require("express");
 var bodyParser=require("body-parser");
 var app = express();
-//var http = require('http');
+var mongoose = require('mongoose');
+
 app.set("view engine", "ejs");
 //app.use(express.static("public"));
 app.use(express.static("images"));
 //app.listen(3000);
-campgrounds=[
-                {name: "Upper Big Bend", image: "Upper Big Bend.jpeg"},
-                {name: "Oeschinen Lake", image: "Oeschinen Lake.jpeg"},
-                {name: "Lac d'Amour", image: "Lac d'Amour.jpeg"},
-		{name: "Upper Big Bend", image: "Upper Big Bend.jpeg"},
-                {name: "Oeschinen Lake", image: "Oeschinen Lake.jpeg"},
-                {name: "Lac d'Amour", image: "Lac d'Amour.jpeg"},
-	 	{name: "Upper Big Bend", image: "Upper Big Bend.jpeg"},
-                {name: "Oeschinen Lake", image: "Oeschinen Lake.jpeg"},
-                {name: "Lac d'Amour", image: "Lac d'Amour.jpeg"}
 
 
-]        
+//DB schema
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+var campgroundSchema= new mongoose.Schema({
+	name:String,
+	image:String
+});
+
+var Campground=mongoose.model("Campground", campgroundSchema);
+
+//Campground.create(
+//	{
+//		name:"Oeschinen Lake", 
+//		image: "Oeschinen Lake.jpeg"
+//	}, function(err, newCampground){
+//		if(err){
+//			console.log("error");
+//			console.log(err);
+//		}else{
+//			console.log("added to DB");
+//			console.log(newCampground);
+//		}
+//	});
 
 
-app.use(bodyParser.urlencoded({extended :true}))
+//campgrounds=[
+//                {name: "Upper Big Bend", image: "Upper Big Bend.jpeg"},
+//                {name: "Oeschinen Lake", image: "Oeschinen Lake.jpeg"},
+//                {name: "Lac d'Amour", image: "Lac d'Amour.jpeg"},
+//		{name: "Upper Big Bend", image: "Upper Big Bend.jpeg"},
+//                {name: "Oeschinen Lake", image: "Oeschinen Lake.jpeg"},
+//                {name: "Lac d'Amour", image: "Lac d'Amour.jpeg"},
+//	 	{name: "Upper Big Bend", image: "Upper Big Bend.jpeg"},
+//                {name: "Oeschinen Lake", image: "Oeschinen Lake.jpeg"},
+//                {name: "Lac d'Amour", image: "Lac d'Amour.jpeg"}
+//
+//
+//]        
+
+
+app.use(bodyParser.urlencoded({extended :true}));
 
 app.get("/", function(req, res){
 	//res.send("Landing Page");
@@ -30,7 +57,14 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds : campgrounds});
+	//res.render("campgrounds", {campgrounds : campgrounds});
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("campgrounds",{campgrounds:allCampgrounds});
+		}
+	});
 
 });
 
@@ -39,8 +73,16 @@ app.post("/campgrounds", function(req, res){
 	var name=req.body.name;
 	var image=req.body.image;
 	var newCampground={name:name, image:image}
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
+	//the next line to the array
+	//campgrounds.push(newCampground);
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		}else{
+			res.redirect("/campgrounds");
+		}
+	});
+	//res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req, res){
