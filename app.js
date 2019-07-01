72db2fa371d4a5e430d6fae68c341e8496a3bf36
mhmdrdwn <mhmdrdwn88@gmail.com>
2019-07-01 02:28:49 +0200

    add logout auth, fix navbar

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(require("express-session")({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user
+	next()
+})
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
@@ -45,7 +49,7 @@ app.get("/campgrounds", function(req,res){
 		if(err){
 			console.log(err)	
 		}else{
-			res.render("campgrounds/index", { campgrounds : campgrounds})
+			res.render("campgrounds/index", { campgrounds : campgrounds, currentUser : req.user})
 		}
 	})
 })
@@ -86,7 +90,7 @@ app.get("/campgrounds/:id",function(req,res){
 
 // Comments routes
 // NEW Route
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
 	Campground.findById(req.params.id, function(err, campground){
 		if(err){
 			console.log(err)
@@ -98,7 +102,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 })
 
 //
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
 	Campground.findById(req.params.id, function(err, campground){
 		if(err){
 			console.log(err)
@@ -144,9 +148,23 @@ app.get("/login", function(req,res){
 })
 
 // log in auth
-app.post("/login", passport.authenticate("local",{successRedirect:"/campgrounds", failureRedirect:"/login"}), function(req, res){
-    
+app.post("/login", passport.authenticate("local",{successRedirect:"/campgrounds", failureRedirect:"/login"}), function(req, res){ 
 }); 
+
+// logout auth
+app.get("/logout", function(req, res){
+	req.logout()
+	res.redirect("/campgrounds")
+})
+
+//middleware
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next()
+	}else{
+		res.redirect("/login")
+	}
+}
 
 app.listen(3000, function(){
 	console.log("server is up")	 
